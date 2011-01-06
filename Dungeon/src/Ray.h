308abb75724d0f7107e549ed213f4926e471be0e
ricardobqueiroz@gmail.com <ricardobqueiroz@gmail.com@de1b78f8-b07c-c201-325b-33e7d83179ed>
2011-01-06 06:04:53 +0000

    funcionando propriamente agora

--- Dungeon/src/Ray.h
+++ Dungeon/src/Ray.h
@@ -13,13 +13,13 @@
 class Shape{
 public:
 	Shape();
-
+	virtual bool hittest(Point3 p, Point3 d, float *l)=0;
 };
 
 class Sphere:public Shape{
 public:
 	Sphere(float x, float y, float z, float r);
-	hittest
+	bool hittest(Point3 p, Point3 d, float *l);
 };
 
 class Ray {
@@ -30,7 +30,7 @@ public:
 	Point3 p,d;
 	float distance;
 
-	bool intersect(Shape s);
+	bool intersect(Shape *s);
 };
 
 #endif /* RAY_H_ */

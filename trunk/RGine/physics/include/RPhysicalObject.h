0af355c582f8eca886da5e3870825c21c47869a4
ricardobqueiroz@gmail.com <ricardobqueiroz@gmail.com@de1b78f8-b07c-c201-325b-33e7d83179ed>
2011-08-02 11:34:34 +0000

    Sorted files

---
+++ trunk/RGine/physics/include/RPhysicalObject.h
@@ -0,0 +1,17 @@
+/*
+ * RPhysicalObject.h
+ *
+ *  Created on: Aug 2, 2011
+ *      Author: ricardo
+ */
+
+#ifndef RPHYSICALOBJECT_H_
+#define RPHYSICALOBJECT_H_
+
+class RPhysicalObject {
+public:
+	RPhysicalObject();
+	virtual ~RPhysicalObject();
+};
+
+#endif /* RPHYSICALOBJECT_H_ */

3f90cd02608a1a0857f7dbbae5371861f9c42dc1
ricardobqueiroz@gmail.com <ricardobqueiroz@gmail.com@de1b78f8-b07c-c201-325b-33e7d83179ed>
2014-08-06 22:54:15 +0000

    gogogo

--- trunk/CG1/scene/scene_voxel.cpp
+++ trunk/CG1/scene/scene_voxel.cpp
@@ -2,26 +2,64 @@
 
 #include <QtOpenGL>
 
+#include "opengl/textures.h"
+
+const int kVoxelCubeDimension = 10;
+const float kVoxelSize = 2.0f / float(kVoxelCubeDimension-1);
+
 SceneVoxel::SceneVoxel():
   Scene()
 {
-  qDebug() << "creating voxel scene";
 }
 
-void SceneVoxel::drawObjects()
+void SceneVoxel::drawObjects() const
 {
-//  qDebug() << "drawing";
+  glPushMatrix();
+  glPushAttrib(GL_ALL_ATTRIB_BITS);
 
-//  glBegin(GL_QUADS);
+  glDisable(GL_CULL_FACE);
 
-//  glVertex3f(0,0,0);
-//  glVertex3f(1,0,0);
-//  glVertex3f(1,1,0);
-//  glVertex3f(0,1,0);
+  glScalef(zoom(),zoom(),zoom());
+  glRotatef(rot_x(),1,0,0);
+  glRotatef(rot_y(),0,1,0);
+  glRotatef(rot_z(),0,0,1);
 
-//  glEnd();
+//  Textures::instance()->set3DTexture("3DTex");
+    Textures::instance()->setTexture("fur.png");
 
+  glBegin(GL_QUADS);
 
+  glNormal3f(0,0,1);
+
+  for(int i=0; i<kVoxelCubeDimension; i++){
+    float layer = 1.0 - kVoxelSize * i;
+    glTexCoord3f(0,0,1);
+    glVertex3f(-1,-1,layer);
+    glTexCoord3f(1,0,1);
+    glVertex3f(+1,-1,layer);
+    glTexCoord3f(1,1,1);
+    glVertex3f(+1,+1,layer);
+    glTexCoord3f(0,1,1);
+    glVertex3f(-1,+1,layer);
+
+//    glTexCoord3f(0,0,0);
+//    glVertex3f(-1,layer,-1);
+//    glTexCoord3f(0,0,1);
+//    glVertex3f(-1,layer,+1);
+//    glTexCoord3f(0,0,0);
+//    glVertex3f(+1,layer,+1);
+//    glVertex3f(+1,layer,-1);
+
+//    glVertex3f(layer,-1,-1);
+//    glVertex3f(layer,+1,-1);
+//    glVertex3f(layer,+1,+1);
+//    glVertex3f(layer,-1,+1);
+  }
+
+  glEnd();
+
+  glPopAttrib();
+  glPopMatrix();
 }
 
 void SceneVoxel::setup_spec()
@@ -30,7 +68,7 @@ void SceneVoxel::setup_spec()
   Model::load(":/model/skydome.obj");
 
   Object *tire1 = Object::create("Tire 1","tire");
-//  tire1->addAnimation(new AnimationSpin(0,10,AnimationSpin::SPIN_X));
+  //  tire1->addAnimation(new AnimationSpin(0,10,AnimationSpin::SPIN_X));
 
   skybox_ = Object::create("Skydome","skydome");
 
